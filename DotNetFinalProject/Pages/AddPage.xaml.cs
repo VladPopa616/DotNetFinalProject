da8aeb916c47fa79f59cbeb5e3f216f2518c1568
lawre <lawre@DESKTOP-K72MI89>
2020-12-09 23:39:38 -0500

    Working on view

--- DotNetFinalProject/Pages/AddPage.xaml.cs
+++ DotNetFinalProject/Pages/AddPage.xaml.cs
@@ -24,5 +24,19 @@ namespace DotNetFinalProject.Pages
         {
             InitializeComponent();
         }
+
+        private void submitCreate(object sender, RoutedEventArgs e)
+        {
+            if (Create_FirstName.Text.Equals(string.Empty) || Create_LastName.Equals(string.Empty) || Create_PhoneNumber.Equals(string.Empty) || Create_Email.Equals(string.Empty))
+            {
+                MessageBox.Show("You are missing a field!", "Error", MessageBoxButton.OK);
+                return;
+            }
+            else
+            {
+                MessageBox.Show("Successfully added a new contact!", "Success", MessageBoxButton.OK);
+            }
+        }
+
     }
 }
