8d2e7ade16b7a0033fb05cfe3c2abf99601a8cce
Vlad Popa <1930022@champlaincollege.qc.ca>
2020-12-12 12:15:35 -0500

    A few modifications in Add event plus an extra tweak on Add Window.

--- DotNetFinalProject/Pages/AddPage.xaml.cs
+++ DotNetFinalProject/Pages/AddPage.xaml.cs
@@ -21,6 +21,7 @@ namespace DotNetFinalProject.Pages
     /// </summary>
     public partial class AddPage : Page
     {
+        SQLCommunications sql = SQLCommunications.Instance;
         public AddPage()
         {
             InitializeComponent();
@@ -34,52 +35,16 @@ namespace DotNetFinalProject.Pages
             }
             else
             {
-                Contact contact = new Contact
-                {
-                    fn = Create_FirstName.Text,
-                    ln = Create_LastName.Text,
-                    email = Create_Email.Text,
-                    phone = Create_PhoneNumber.Text
-                };
+                Contact contact = new Contact(Create_Id.Text, Create_FirstName.Text, Create_LastName.Text, Create_Email.Text, Create_PhoneNumber.Text);
+                sql.AddContacts(contact);
+               
 
                 MessageBox.Show(contact.fn, "Success!", MessageBoxButton.OK);
-                Create(contact);
+                
 
-                MessageBox.Show("Your contact has been added to ", "Success!", MessageBoxButton.OK);
+                MessageBox.Show("Your contact has been added to the database", "Success!", MessageBoxButton.OK);
             }
         }
 
-        public int Create(Contact contact)
-        {
-            var con = new SqlConnection(@"data source=DESKTOP-K72MI89\SQLEXPRESS2;database = Contact;Trusted_Connection=True");
-
-            SqlCommand cm = new SqlCommand("insert into contact(FirstName, LastName, Email, PhoneNumber) values(@FirstName, @LastName, @Email, @PhoneNumber)", con);
-
-            cm.Parameters.AddWithValue("@FirstName", contact.fn);
-            cm.Parameters.AddWithValue("@LastName", contact.ln);
-            cm.Parameters.AddWithValue("@Email", contact.email);
-            cm.Parameters.AddWithValue("@PhoneNumber", contact.phone);
-
-            int newId = 0;
-            try
-            {
-                con.Open();
-                cm.ExecuteNonQuery();
-                string query2 = "Select @@Identity as newId from contact";
-                cm.CommandText = query2;
-                cm.Connection = con;
-                newId = Convert.ToInt32(cm.ExecuteScalar());
-            }
-            catch (SqlException e)
-            {
-                Console.WriteLine("Error inserting data");
-            }
-            finally
-            {
-                con.Close();
-            }
-
-            return newId;
-        }
     }
 }
